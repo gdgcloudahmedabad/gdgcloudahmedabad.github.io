41ee303a02ea6a8fb1fe705092a08ea61e353e1b
harshcrop <harshcrop@gmail.com>
2019-11-13 00:49:51 +0530

    fix email id & fb broken links

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.613aeaf1bf57d2770cbee16c339c0c3f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.6345b4f05f10b5ef7b70435ad20d7869.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
